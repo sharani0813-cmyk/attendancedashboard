1b278ceb9e8d44713f1a1b69e58be683980fce0f
sharani0813-cmyk <sharani0813@gmail.com>
2026-07-02 11:29:33 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,5 +5,6 @@ export const firebaseConfig = {
     storageBucket: "attendancedashboard-d2d5c.firebasestorage.app",
     messagingSenderId: "113558043463",
     appId: "1:113558043463:web:f38e772949956d592aea62",
+    measurementId: "G-6882L8EXTL"
 };
 
