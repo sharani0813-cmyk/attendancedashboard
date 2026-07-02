dcbad70198cc257458b81611bf70b7a2341974ab
sharani0813-cmyk <sharani0813@gmail.com>
2026-07-02 11:06:03 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,7 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCB8nIa8oULKIMIrKqQ9IUbJWOmQp-kjGQ",
+    authDomain: "attendancedashboard-d2d5c.firebaseapp.com",
+    projectId: "attendancedashboard-d2d5c",
+    storageBucket: "attendancedashboard-d2d5c.firebasestorage.app",
+    messagingSenderId: "113558043463",
+    appId: "1:113558043463:web:f38e772949956d592aea62"
